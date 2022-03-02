fb50e5ddba08fe4f3ce767d16da8e29b9c4e95a4
LamkadamNabil <na.lamkadam@gmailcom>
2022-03-02 01:19:37 +0100

    Header and About Done

--- src/client.js
+++ src/client.js
@@ -2,7 +2,7 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-  projectId: 'fij0fiij',
+  projectId: '',
   dataset: 'production',
   apiVersion: '2022-02-01',
   useCdn: true,
